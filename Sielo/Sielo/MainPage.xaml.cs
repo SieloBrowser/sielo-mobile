a12e781c13f2a32cd84a262f5c251bcacff653d3
Feldrise <admin@feldrise.com>
2018-07-06 15:16:23 +0200

    [Add] Action bar

--- Sielo/Sielo/MainPage.xaml.cs
+++ Sielo/Sielo/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sielo.Views;
 using Xamarin.Forms;
 
 namespace Sielo
@@ -12,6 +13,19 @@ namespace Sielo
 		public MainPage()
 		{
 			InitializeComponent();
+
+		    webView.Navigating += PageLoading;
+		    actionBar.LoadRequest += OnLoadRequest;
 		}
+
+	    void PageLoading(object sender, WebNavigatingEventArgs e)
+	    {
+            actionBar.SetUrl(e.Url);
+	    }
+
+	    void OnLoadRequest(object sender, LoadRequestEventArgs e)
+	    {
+	        webView.Source = e.Url;
+	    }
 	}
 }
---
+++ Sielo/Sielo/Views/SActionBar.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sielo.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SActionBar : ContentView
+	{
+	    public event EventHandler<LoadRequestEventArgs> LoadRequest;
+
+		public SActionBar ()
+		{
+			InitializeComponent ();
+
+		    urlBar.SearchButtonPressed += OnUrlEntered;
+		}
+
+	    public void SetUrl(string url)
+	    {
+	        urlBar.Text = url;
+	    }
+
+	    private void OnUrlEntered(object sender, EventArgs args)
+	    {
+	        var url = urlBar.Text;
+
+            // TODO: differencie search and url
+
+	        if (!url.StartsWith("http") || !url.StartsWith("https"))
+	            url = "https://" + url;
+
+	        var loadRequestEventArgs = new LoadRequestEventArgs
+	        {
+	            Url = url
+	        };
+
+	        var handler = LoadRequest;
+	        handler?.Invoke(this, loadRequestEventArgs);
+
+	    }
+	}
+
+    public class LoadRequestEventArgs : EventArgs
+    {
+        public string Url { get; set; }
+    }
+}
