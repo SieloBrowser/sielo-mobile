29b2ae3fae6024891c5d76d657979dc73ca057b1
Feldrise <admin@feldrise.com>
2018-07-06 13:09:26 +0200

    [Add] Settings

---
+++ Sielo/Sielo/Helpers/Settings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Plugin.Settings;
+using Plugin.Settings.Abstractions;
+
+namespace Sielo.Helpers
+{
+    public static class Settings
+    {
+        private static ISettings AppSettings => CrossSettings.Current;
+
+        public static string HomePage
+        {
+            get => AppSettings.GetValueOrDefault("home_page", "https://doosearch.sielo.app");
+            set => AppSettings.AddOrUpdateValue("home_page", value);
+        }
+    }
+}
