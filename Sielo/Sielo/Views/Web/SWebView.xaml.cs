f8d013bb0bc1aa171d82d5cf178ec29edc543bde
Feldrise <admin@feldrise.com>
2018-07-06 13:28:26 +0200

    [Add] SWebView

---
+++ Sielo/Sielo/Views/Web/SWebView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sielo.Views.Web
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SWebView : WebView
+	{
+		public SWebView ()
+		{
+			InitializeComponent ();
+
+		    Source = Sielo.Helpers.Settings.HomePage;
+		}
+	}
+}
